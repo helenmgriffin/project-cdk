0f1f3294bdae5d99bc9e36b2292d0ee8b32ac97a
helenmgriffin <77994420+helenmgriffin@users.noreply.github.com>
2021-03-19 10:49:20 +0000

    commit new project repo stack

---
+++ src/ProjectCdk/PipelineStack.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.CodeCommit;
+
+namespace ProjectCdk
+{
+    public class ProjectPipelineStack: Stack
+    {
+        public ProjectPipelineStack(Construct parent, string id, IStackProps props = null) : base(parent, id, props)
+        {
+            // Creates a CodeCommit repository called 'ProjectRepo'
+            var repo = new Repository(this, "ProjectRepo", new RepositoryProps
+            {
+                RepositoryName = "ProjectRepo"
+            });
+
+            // Pipeline code goes here
+        }
+    }
+}

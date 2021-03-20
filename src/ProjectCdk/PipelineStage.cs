1bccff4129aea2b9ac3f4e13bef59a74d0686337
helenmgriffin <77994420+helenmgriffin@users.noreply.github.com>
2021-03-20 17:43:42 +0000

    Add deploy stage to pipeline

---
+++ src/ProjectCdk/PipelineStage.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.Pipelines;
+
+namespace ProjectCdk
+{
+    public class ProjectPipelineStage : Stage
+    {
+        public ProjectPipelineStage(Construct scope, string id, StageProps props = null) : base(scope , id, props)
+        {
+            //this declares a new Stage(component of a pipeline), and in that stage instantiate our application stack.(ProjectCdkStack)
+            var service = new ProjectCdkStack(this, "WebService");
+        }
+    }
+}

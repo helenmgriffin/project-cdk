40f18ccfe749dad7a2d51d0159d75fe44e2b1013
helenmgriffin <77994420+helenmgriffin@users.noreply.github.com>
2021-03-22 12:12:34 +0000

    secret fix

--- src/ProjectCdk/PipelineStack.cs
+++ src/ProjectCdk/PipelineStack.cs
@@ -34,7 +34,10 @@ namespace ProjectCdk
                 {
                     ActionName = "GitHub",
                     Output = sourceArtifact,
-                    OauthToken = SecretValue.SecretsManager("GitHubPersonalAccessToken"), //.PlainText("a9535df8d5185be0c2644a5247d35c97c601d9d5"), //("GitHubPersonalAccessToken"), //("GitHubPersonalAccessToken", "1"), 
+                    OauthToken = SecretValue.SecretsManager("arn:aws:secretsmanager:eu-west-1:235629185262:secret:GitHubPersonalAccessToken-MSZRLO", new SecretsManagerSecretOptions
+                    {
+                        JsonField = "GitHubPersonalAccessToken"
+                    }), //.PlainText("a9535df8d5185be0c2644a5247d35c97c601d9d5"), //("GitHubPersonalAccessToken"), //("GitHubPersonalAccessToken", "1"), 
                     Trigger = GitHubTrigger.WEBHOOK,
                     // Replace these with your actual GitHub project name
                     Owner = "helenmgriffin",
@@ -59,7 +62,7 @@ namespace ProjectCdk
                     InstallCommand = "npm install -g aws-cdk && wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb && dpkg -i packages-microsoft-prod.deb && apt-get update && apt-get install -y apt-transport-https && apt-get update && apt-get install -y dotnet-sdk-3.1",
                     BuildCommand = "dotnet build src", // Language-specific build cmd
                 })
-            });;
+            }); 
 
             //create an instance of the stage 
             var deploy = new ProjectPipelineStage(this, "Deploy");
--- src/ProjectCdk/Program.cs
+++ src/ProjectCdk/Program.cs
@@ -1,11 +1,14 @@
 ﻿using Amazon.CDK;
 
+
 namespace ProjectCdk
 {
     sealed class Program
     {
         public static void Main(string[] args)
         {
+            //var secret = Helper.GetSecret();
+
             //change the entry point to deploy our pipeline. no longer want the main CDK application to deploy the original app.
             var app = new App();
             new ProjectPipelineStack(app, "ProjectPipelineStack");
@@ -16,5 +19,9 @@ namespace ProjectCdk
 
             //app.Synth();
         }
+
+
+
+       
     }
 }
